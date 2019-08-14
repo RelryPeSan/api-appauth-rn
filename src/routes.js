e7f53945f2c4be2919b0be04d17a1dc0ad74fec9
Relry Pereira dos Santos <relrypesan@gmail.com>
2019-08-14 04:11:39 -0300

    Update

--- src/routes.js
+++ src/routes.js
@@ -5,7 +5,7 @@ const routes = express.Router();
 
 
 routes.get('/', (req, res) => {
-    return res.json({response: 'success'});
+    return res.json({response: 'success', portNumber: (process.env.PORT || 8080)});
 });
 
 // Usuario
